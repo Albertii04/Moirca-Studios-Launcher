573738f5a8c05af4fab7c17234263f6dfecdbabd
albertii04 <albertmarimon04@gmail.com>
2023-03-17 03:02:47 +0100

    Finally we have an image

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1455,7 +1455,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/Albertii04/Moirca-Studios-Launcher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
